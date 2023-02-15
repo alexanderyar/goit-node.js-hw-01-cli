4aec3141c5025a512a604b7a62c755c9cfc58eaf
alexanderyar <103504393+alexanderyar@users.noreply.github.com>
2023-02-15 12:22:14 -0700

    deleted comments

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-// importing Commander for cli experience
 const { Command } = require("commander");
 const program = new Command();
 
-// importing ALL the functions from contacts.js
-const contacts = require("./contacts")
+const contacts = require("./contacts");
 
-// file settings for Commander
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -13,34 +10,32 @@ program
   .option("-e, --email <type>", "user email")
   .option("-p, --phone <type>", "user phone");
 
-
 program.parse(process.argv);
 
 const argv = program.opts();
 
-console.log(argv)
-
+console.log(argv);
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-          const allContacts = await contacts.listContacts();
-          console.table(allContacts)
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
       break;
 
     case "get":
-          const byId = await contacts.getContactById(id)
-          console.log(byId)
+      const byId = await contacts.getContactById(id);
+      console.log(byId);
       break;
 
     case "add":
-          const addingContact = await contacts.addContact( name, email, phone )
-          console.log(addingContact)
+      const addingContact = await contacts.addContact(name, email, phone);
+      console.log(addingContact);
       break;
 
     case "remove":
-          const removedContact = await contacts.removeContact(id)
-          console.log(removedContact)
+      const removedContact = await contacts.removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
